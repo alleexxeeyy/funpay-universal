6788e4c032bfb9ed847c474ba218c059a0e200f9
alleexxeeyy <flizkapodlizka@gmail.com>
2025-05-27 15:24:27 +0500

    1.1.0 - fixes

--- services/updater.py
+++ services/updater.py
@@ -29,11 +29,13 @@ class Updater:
                 return False
             print(f"\n{Fore.LIGHTYELLOW_EX}Доступна новая версия: {Fore.LIGHTWHITE_EX}{latest_version}"
                     f"\n{Fore.WHITE}Скачиваем: {Fore.LIGHTWHITE_EX}{latest_release['html_url']}\n")
+            print(f"{Fore.WHITE}Загружаю обновление...")
             bytes = Updater.download_update(latest_release)
             if bytes:
+                print(f"{Fore.WHITE}Устанавливаю обновление...")
                 if Updater.install_update(bytes):
-                    print(f"{Fore.LIGHTYELLOW_EX}✅ Обновление {Fore.LIGHTWHITE_EX}{latest_version} {Fore.LIGHTYELLOW_EX}было успешно установлено.")
-                    print(f"{Fore.WHITE}Перезапуск бота...")
+                    print(f"\n{Fore.LIGHTYELLOW_EX}✅ Обновление {Fore.LIGHTWHITE_EX}{latest_version} {Fore.LIGHTYELLOW_EX}было успешно установлено.")
+                    print(f"{Fore.WHITE}Перезапуск бота...\n")
                     os.execv(sys.executable, [sys.executable] + sys.argv)
         except Exception as e:
             print(f"{Fore.LIGHTRED_EX}При проверке на наличие обновлений произошла ошибка: {Fore.WHITE}{e}")
@@ -41,7 +43,7 @@ class Updater:
 
     @staticmethod
     def download_update(release_info: str):
-        """ Скачивает архив с обновлением """
+        """ Скачивает архив с обновлением. """
         try:
             zip_url = release_info['zipball_url']
             zip_response = requests.get(zip_url)
@@ -54,28 +56,31 @@ class Updater:
     
     @staticmethod
     def install_update(zip_response_content: bytes):
-        """ Устанавливает обновление из архива """
+        """Устанавливает обновление."""
+        temp_dir = ".temp_update"
         try:
             with zipfile.ZipFile(io.BytesIO(zip_response_content), 'r') as zip_ref:
-                current_dir = "."
-                temp_dir = ".temp_update"
-                os.makedirs(temp_dir, exist_ok=True)
-
                 zip_ref.extractall(temp_dir)
-                update_files = []
-                for root, _, files in os.walk(temp_dir):
-                    for file in files:
-                        rel_path = os.path.relpath(os.path.join(root, file), temp_dir)
-                        update_files.append(rel_path)
                 
-                for file in update_files:
-                    src = os.path.join(temp_dir, file)
-                    dst = os.path.join(current_dir, file)
-                    os.makedirs(os.path.dirname(dst), exist_ok=True)
-                    shutil.copy2(src, dst)
-
-                shutil.rmtree(temp_dir)
-            return True
+                archive_root = None
+                for item in os.listdir(temp_dir):
+                    if os.path.isdir(os.path.join(temp_dir, item)):
+                        archive_root = os.path.join(temp_dir, item)
+                        break
+                if not archive_root:
+                    raise Exception("В архиве нет корневой папки!")
+                
+                for root, _, files in os.walk(archive_root):
+                    for file in files:
+                        src = os.path.join(root, file)
+                        dst = os.path.join('.', os.path.relpath(src, archive_root))
+                        os.makedirs(os.path.dirname(dst), exist_ok=True)
+                        shutil.copy2(src, dst)
+                
+                return True
         except Exception as e:
             print(f"{Fore.LIGHTRED_EX}При установке обновления произошла ошибка: {Fore.WHITE}{e}")
             return False
+        finally:
+            if os.path.exists(temp_dir):
+                shutil.rmtree(temp_dir, ignore_errors=True)
